c5276a5190107fc166400c944ab1868a40cc985d
Steven Imus <imussg@gmail.com>
2018-09-17 21:40:26 -0400

    changed database url global variable; hopefully fixes problem.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
-console.log(process.env);
+
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/gardening-app',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://dev:devdev1@ds119422.mlab.com:19422/gardening-app',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/gardening-app-test'
   // TEST_DATABASE_URL:
   //     process.env.TEST_DATABASE_URL ||
