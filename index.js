8e501a992967ac6fcf2215ce6a72833bec25442e
Steven Imus <imussg@gmail.com>
2018-09-20 13:43:09 -0400

    refixed cors

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ app.use(
 );
 
 // CORS
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(204);
-//   }
-//   next();
-// });
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
 
 app.use('/api/gardens', gardensRouter);
 app.use('/api/plots', plotsRouter);
